0c173ebfcf8857b9b69d55bee6cdee1583a63fb0
ItsAlex <me@itsalex.fr>
2020-07-13 17:48:20 +0200

    feat: add region option to allow connection to AWS

--- index.js
+++ index.js
@@ -6,23 +6,38 @@ const { parse } = require('querystring');
 
 Minio.Client.prototype.getObject = util.promisify(Minio.Client.prototype.getObject);
 
-const serve = ({endPoint, port, useSSL, accessKey, secretKey, bucketName}) => {
-    const minioClient = new Minio.Client(Object.assign(
-        {
-            endPoint: '127.0.0.1',
-            port: 9000,
-            useSSL: false,
-            accessKey: 'admin',
-            secretKey: 'password'
-        },
-        {
-            endPoint,
-            port,
-            useSSL,
-            accessKey,
-            secretKey
-        }
-    ));
+const serve = ({
+  endPoint,
+  port,
+  useSSL,
+  accessKey,
+  secretKey,
+  bucketName,
+  region
+}) => {
+    const connectionInfos = Object.assign(
+      {
+        endPoint: '127.0.0.1',
+        useSSL: false,
+        accessKey: 'admin',
+        secretKey: 'password'
+      },
+      {
+        endPoint,
+        port,
+        useSSL,
+        accessKey,
+        secretKey
+      }
+    );
+    if (port) {
+      connectionInfos.port = port;
+    }
+    if (region) {
+      connectionInfos.region = region;
+    }
+
+    const minioClient = new Minio.Client(connectionInfos);
 
     return async function serve(ctx, next) {
         if (ctx.request.method === 'GET') {
